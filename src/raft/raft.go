4238946e5db503773c390f9fd2e9b97355c5a09a
Valentin Zambelli <valentin.zambelli@gmail.com>
2021-01-01 12:51:24 +1100

    Working Lab 2A

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -17,14 +17,18 @@ package raft
 //   in the same server.
 //
 
-import "sync"
-import "sync/atomic"
-import "../labrpc"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"../labrpc"
+)
 
 // import "bytes"
 // import "../labgob"
-
-
 
 //
 // as each Raft peer becomes aware that successive log entries are
@@ -43,11 +47,9 @@ type ApplyMsg struct {
 	CommandIndex int
 }
 
-//
-// A Go object implementing a single Raft peer.
-//
+// Raft is a Go object implementing a single Raft peer.
 type Raft struct {
-	mu        sync.Mutex          // Lock to protect shared access to this peer's state
+	mu        sync.RWMutex        // Lock to protect shared access to this peer's state
 	peers     []*labrpc.ClientEnd // RPC end points of all peers
 	persister *Persister          // Object to hold this peer's persisted state
 	me        int                 // this peer's index into peers[]
@@ -57,16 +59,84 @@ type Raft struct {
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
 
+	electionTimeout     time.Duration // Timeout for leader heartbeat. Calls for election if timeout reached
+	electionTimeoutBase time.Duration // Time between checking if a leader election is necessary
+	lastHeardFromLeader time.Time     // Time since the last AppendEntry message has arrived from the leader
+
+	term     int  // latest term server has seen (initialized to 0 on first boot, increases monotonically)
+	votedFor int  // candidateId that received vote in current term (or null if none)
+	isLeader bool // whether this peer thinks it's the leader of a cluster
 }
 
-// return currentTerm and whether this server
-// believes it is the leader.
-func (rf *Raft) GetState() (int, bool) {
+// Write locked access methods
+func (rf *Raft) resetElectionTimeout() {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
 
-	var term int
-	var isleader bool
-	// Your code here (2A).
-	return term, isleader
+	rf.lastHeardFromLeader = time.Now()
+}
+
+func (rf *Raft) updateTerm(newTerm int) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	rf.votedFor = -1
+	rf.term = newTerm
+}
+
+func (rf *Raft) advanceTerm() {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	rf.votedFor = rf.me
+	rf.term++
+}
+
+func (rf *Raft) grantVote(votee int) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	rf.votedFor = votee
+}
+
+func (rf *Raft) demoteToFollower() {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	rf.isLeader = false
+}
+
+func (rf *Raft) promoteToLeader() {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	rf.isLeader = true
+}
+
+// Read locked access functions
+func (rf *Raft) shouldCallElection() (bool, time.Duration) {
+	rf.mu.RLock()
+	defer rf.mu.RUnlock()
+
+	t := time.Now()
+	elapsed := t.Sub(rf.lastHeardFromLeader)
+
+	return elapsed < rf.electionTimeout || rf.isLeader, elapsed
+}
+
+// GetState returns relevant state information
+func (rf *Raft) getState() (int, bool, int) {
+	rf.mu.RLock()
+	defer rf.mu.RUnlock()
+
+	return rf.term, rf.isLeader, rf.votedFor
+}
+
+func (rf *Raft) GetState() (int, bool) {
+	rf.mu.RLock()
+	defer rf.mu.RUnlock()
+
+	return rf.term, rf.isLeader
 }
 
 //
@@ -84,7 +154,6 @@ func (rf *Raft) persist() {
 	// data := w.Bytes()
 	// rf.persister.SaveRaftState(data)
 }
-
 
 //
 // restore previously persisted state.
@@ -108,81 +177,243 @@ func (rf *Raft) readPersist(data []byte) {
 	// }
 }
 
-
-
-
-//
-// example RequestVote RPC arguments structure.
-// field names must start with capital letters!
-//
+// RequestVoteArgs are passed to the RequestVote RPC method
 type RequestVoteArgs struct {
 	// Your data here (2A, 2B).
+	Term        int
+	CandidateID int
 }
 
-//
-// example RequestVote RPC reply structure.
-// field names must start with capital letters!
-//
+// RequestVoteReply is filled in by the RequestVote RPC method
 type RequestVoteReply struct {
 	// Your data here (2A).
+	Term        int
+	VoteGranted bool
 }
 
-//
-// example RequestVote RPC handler.
-//
-func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
+// RequestVote is called by the RPC framework to request this raft instance's vote in a leader election
+func (rf *Raft) RequestVote(message *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (2A, 2B).
+
+	term, _ := rf.GetState()
+
+	rf.resetElectionTimeout()
+	// If we are in a new epoch some instance just became leader
+	// Make sure this instance is demoted to a follower since a new leader is sending messages
+	if message.Term > term {
+		println(fmt.Sprintf("Instance %d saw new term %d, updated its old term %d", rf.me, message.Term, term))
+
+		rf.updateTerm(message.Term)
+		rf.demoteToFollower()
+	}
+
+	reply.Term = term
+	shouldGrantVote := rf.shouldGrantVote(message)
+
+	reply.VoteGranted = shouldGrantVote
+
+	if !shouldGrantVote {
+		rf.mu.RLock()
+		println(fmt.Sprintf("Instance %d refused vote request from candidate %d: instance term %d, has voted for %d", rf.me, message.CandidateID, rf.term, rf.votedFor))
+		rf.mu.RUnlock()
+	} else {
+		rf.grantVote(message.CandidateID)
+
+		rf.mu.RLock()
+		println(fmt.Sprintf("Instance %d received vote request from candidate %d for term %d and granted the vote", rf.me, message.CandidateID, message.Term))
+		rf.mu.RUnlock()
+
+	}
+
 }
 
-//
-// example code to send a RequestVote RPC to a server.
-// server is the index of the target server in rf.peers[].
-// expects RPC arguments in args.
-// fills in *reply with RPC reply, so caller should
-// pass &reply.
-// the types of the args and reply passed to Call() must be
-// the same as the types of the arguments declared in the
-// handler function (including whether they are pointers).
-//
-// The labrpc package simulates a lossy network, in which servers
-// may be unreachable, and in which requests and replies may be lost.
-// Call() sends a request and waits for a reply. If a reply arrives
-// within a timeout interval, Call() returns true; otherwise
-// Call() returns false. Thus Call() may not return for a while.
-// A false return can be caused by a dead server, a live server that
-// can't be reached, a lost request, or a lost reply.
-//
-// Call() is guaranteed to return (perhaps after a delay) *except* if the
-// handler function on the server side does not return.  Thus there
-// is no need to implement your own timeouts around Call().
-//
-// look at the comments in ../labrpc/labrpc.go for more details.
-//
-// if you're having trouble getting RPC to work, check that you've
-// capitalized all field names in structs passed over RPC, and
-// that the caller passes the address of the reply struct with &, not
-// the struct itself.
-//
-func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
+func (rf *Raft) shouldGrantVote(args *RequestVoteArgs) bool {
+	rf.mu.RLock()
+	defer rf.mu.RUnlock()
+
+	termIsCurrent := !(args.Term < rf.term)
+	noConflictingVotes := rf.votedFor == -1 || rf.votedFor == args.CandidateID
+
+	shouldGrantVote := termIsCurrent && noConflictingVotes
+
+	return shouldGrantVote
+}
+
+func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply, resultCh chan bool) bool {
 	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
+	term, _ := rf.GetState()
+
+	if ok {
+		if reply.Term > term {
+
+			rf.mu.RLock()
+			println(fmt.Sprintf("Instance %d saw new term %d, updated its old term %d", rf.me, reply.Term, rf.term))
+			rf.mu.RUnlock()
+
+			rf.updateTerm(reply.Term)
+			rf.demoteToFollower()
+		}
+
+		if reply.VoteGranted {
+			rf.mu.RLock()
+			println(fmt.Sprintf("Instance %d granted vote by instance %d", rf.me, server))
+			rf.mu.RUnlock()
+
+		}
+
+		resultCh <- reply.VoteGranted
+	}
 	return ok
 }
 
+// AppendEntryArgs is sent to peers by the leader to initate the commit of a new log entry
+// or periodically as a heartbeat
+type AppendEntryArgs struct {
+	Term     int
+	LeaderID int
+}
 
-//
-// the service using Raft (e.g. a k/v server) wants to start
-// agreement on the next command to be appended to Raft's log. if this
-// server isn't the leader, returns false. otherwise start the
-// agreement and return immediately. there is no guarantee that this
-// command will ever be committed to the Raft log, since the leader
-// may fail or lose an election. even if the Raft instance has been killed,
-// this function should return gracefully.
-//
-// the first return value is the index that the command will appear at
-// if it's ever committed. the second return value is the current
-// term. the third return value is true if this server believes it is
-// the leader.
-//
+// AppendEntryReply is filled in by peers to inform the leader that they have received a new log entry
+type AppendEntryReply struct {
+	Term    int
+	Success bool
+}
+
+// AppendEntry is called by the RPC framework to add new log entries
+func (rf *Raft) AppendEntry(message *AppendEntryArgs, reply *AppendEntryReply) {
+
+	// Your code here (2A, 2B).
+	rf.resetElectionTimeout()
+	term, _ := rf.GetState()
+
+	// if some older leader sends messages, disregard
+	if message.Term < term {
+		reply.Term = term
+		reply.Success = false
+
+		return
+	}
+
+	// If we are in a new epoch some instance just became leader
+	// Make sure this instance is demoted to a follower since a new leader is sending messages
+	if message.Term > term {
+		println(fmt.Sprintf("Instance %d saw new term %d, updated its old term %d", rf.me, message.Term, term))
+
+		rf.updateTerm(message.Term)
+		rf.demoteToFollower()
+	}
+
+	reply.Term = term
+	reply.Success = true
+}
+
+func (rf *Raft) sendAppendEntry(server int, args *AppendEntryArgs, reply *AppendEntryReply) bool {
+	ok := rf.peers[server].Call("Raft.AppendEntry", args, reply)
+	term, _ := rf.GetState()
+
+	// Demote to follower because instances exist with a higher term
+	if ok && reply.Term > term {
+		rf.updateTerm(reply.Term)
+		rf.demoteToFollower()
+
+		rf.mu.RLock()
+		println(fmt.Sprintf("Instance %d was demoted as leader because term has advanced", rf.me))
+		rf.mu.RUnlock()
+	}
+	return ok
+}
+
+func (rf *Raft) maybeSendHeartbeat() {
+	for {
+		rf.mu.RLock()
+		if rf.isLeader && !rf.killed() {
+			rf.sendHeartbeat()
+		}
+		rf.mu.RUnlock()
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+func (rf *Raft) sendHeartbeat() {
+	args := &AppendEntryArgs{}
+	args.Term = rf.term
+	args.LeaderID = rf.me
+	for i := range rf.peers {
+		if i != rf.me {
+			reply := &AppendEntryReply{}
+			go rf.sendAppendEntry(i, args, reply)
+		}
+	}
+}
+
+func (rf *Raft) maybeRequestVote() {
+	for {
+		shouldCallVote, elapsed := rf.shouldCallElection()
+		if shouldCallVote {
+			time.Sleep(10 * time.Millisecond)
+			continue
+		}
+
+		// Set up this instance for leader election
+		votesReceived := 1
+		majority := (len(rf.peers) / 2) + 1
+		rf.advanceTerm()
+		rf.mu.RLock()
+		me := rf.me
+		term := rf.term
+		rf.mu.RUnlock()
+		println(fmt.Sprintf("Instance %d initiated leader vote for new term %d, haven't heard from current leader in %dms", me, term, elapsed.Milliseconds()))
+
+		args := &RequestVoteArgs{}
+		args.Term = rf.term
+		args.CandidateID = rf.me
+
+		tally := make(chan bool)
+
+		// Request vote from all peers
+		for i := range rf.peers {
+			if i != rf.me {
+				reply := &RequestVoteReply{}
+				go rf.sendRequestVote(i, args, reply, tally)
+			}
+		}
+
+		// Wait for votes until a majority has voted
+		// If no peer responds after 1 second skip
+		for j := 1; j < majority; j++ {
+			select {
+			case vote := <-tally:
+				if vote {
+					votesReceived++
+				}
+			case <-time.After(1 * time.Second):
+				println(fmt.Sprintf("Instance %d vote tally timed out", me))
+
+				continue
+			}
+
+		}
+
+		rf.resetElectionTimeout()
+
+		// maybe make this instance leader
+		if votesReceived >= majority {
+			println(fmt.Sprintf("Instance %d number of votes %d", me, votesReceived))
+
+			rf.promoteToLeader()
+			go rf.sendHeartbeat()
+			println(fmt.Sprintf("Instance %d became leader", me))
+		} else {
+			println(fmt.Sprintf("Instance %d failed to become leader", me))
+		}
+
+		// Back off to minimise collision in case of failed election
+		backoff, _ := time.ParseDuration(fmt.Sprintf("%dms", rand.Intn(20)))
+		time.Sleep(backoff)
+	}
+}
+
+// Start will initiate a vote to commit a new log entry
 func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	index := -1
 	term := -1
@@ -190,21 +421,10 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 
 	// Your code here (2B).
 
-
 	return index, term, isLeader
 }
 
-//
-// the tester doesn't halt goroutines created by Raft after each test,
-// but it does call the Kill() method. your code can use killed() to
-// check whether Kill() has been called. the use of atomic avoids the
-// need for a lock.
-//
-// the issue is that long-running goroutines use memory and may chew
-// up CPU time, perhaps causing later tests to fail and generating
-// confusing debug output. any goroutine with a long-running loop
-// should call killed() to check whether it should stop.
-//
+// Kill will prevent this instance from participating in the cluster
 func (rf *Raft) Kill() {
 	atomic.StoreInt32(&rf.dead, 1)
 	// Your code here, if desired.
@@ -215,8 +435,19 @@ func (rf *Raft) killed() bool {
 	return z == 1
 }
 
-//
-// the service or tester wants to create a Raft server. the ports
+func (rf *Raft) logState() {
+	for {
+		if rf.killed() {
+			continue
+		}
+		rf.mu.Lock()
+		println(fmt.Sprintf("STATE Instance %d, isLeader %t, current term %d, last voted for %d", rf.me, rf.isLeader, rf.term, rf.votedFor))
+		rf.mu.Unlock()
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// Make is called the service or tester wants to create a Raft server. the ports
 // of all the Raft servers (including this one) are in peers[]. this
 // server's port is peers[me]. all the servers' peers[] arrays
 // have the same order. persister is a place for this server to
@@ -225,19 +456,31 @@ func (rf *Raft) killed() bool {
 // tester or service expects Raft to send ApplyMsg messages.
 // Make() must return quickly, so it should start goroutines
 // for any long-running work.
-//
 func Make(peers []*labrpc.ClientEnd, me int,
 	persister *Persister, applyCh chan ApplyMsg) *Raft {
 	rf := &Raft{}
 	rf.peers = peers
 	rf.persister = persister
 	rf.me = me
+	rf.votedFor = -1
+	rf.term = 0
 
 	// Your initialization code here (2A, 2B, 2C).
+	electionTimeoutBase, _ := time.ParseDuration(fmt.Sprintf("%dms", 150))
+	electionTimeoutJitter, _ := time.ParseDuration(fmt.Sprintf("%dms", rand.Intn(50)))
+	rf.electionTimeout = electionTimeoutBase + electionTimeoutJitter
+	rf.electionTimeoutBase = electionTimeoutBase
+	rf.lastHeardFromLeader = time.Now()
+
+	println(fmt.Sprintf("Creating instance with ID %d, election timeout %d", me, rf.electionTimeout.Milliseconds()))
+
+	// Background tasks
+	// go rf.logState()
+	go rf.maybeSendHeartbeat()
+	go rf.maybeRequestVote()
 
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
-
 
 	return rf
 }
--- src/raft/test_test.go
+++ src/raft/test_test.go
@@ -8,12 +8,14 @@ package raft
 // test with the original before submitting.
 //
 
-import "testing"
-import "fmt"
-import "time"
-import "math/rand"
-import "sync/atomic"
-import "sync"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
 
 // The tester generously allows solutions to complete elections in one second
 // (much more than the paper's range of timeouts).
@@ -60,27 +62,35 @@ func TestReElection2A(t *testing.T) {
 	leader1 := cfg.checkOneLeader()
 
 	// if the leader disconnects, a new one should be elected.
+	println("Dropped first leader")
 	cfg.disconnect(leader1)
 	cfg.checkOneLeader()
 
 	// if the old leader rejoins, that shouldn't
 	// disturb the new leader.
+	println("Rejoined first leader")
 	cfg.connect(leader1)
 	leader2 := cfg.checkOneLeader()
 
 	// if there's no quorum, no leader should
 	// be elected.
-	cfg.disconnect(leader2)
-	cfg.disconnect((leader2 + 1) % servers)
+	println("Dropped majority of servers")
+	toDrop1 := leader2
+	toDrop2 := (leader2 + 1) % servers
+	fmt.Printf("Dropping %d and %d\n", toDrop1, toDrop2)
+	cfg.disconnect(toDrop1)
+	cfg.disconnect(toDrop2)
 	time.Sleep(2 * RaftElectionTimeout)
 	cfg.checkNoLeader()
 
-	// if a quorum arises, it should elect a leader.
-	cfg.connect((leader2 + 1) % servers)
+	// // if a quorum arises, it should elect a leader.
+	println("Reconnect one server to allow election")
+	cfg.connect(toDrop2)
 	cfg.checkOneLeader()
 
 	// re-join of last node shouldn't prevent leader from existing.
-	cfg.connect(leader2)
+	println("Rejoin old leader")
+	cfg.connect(toDrop1)
 	cfg.checkOneLeader()
 
 	cfg.end()
